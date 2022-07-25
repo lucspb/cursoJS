99664a2040fe969f01b357ec89cb3c231e0a6909
Lucas Estanislau <lucasestanislau@hotmail.com>
2022-07-25 14:25:20 -0300

    definicoes sobre var, let e const

--- capitulo01/demo01-variaveis.js
+++ capitulo01/demo01-variaveis.js
@@ -7,3 +7,15 @@ poder = "rico"
 console.log(
     `Nome: ${nome}, idade: ${idade}, poder: ${poder}`
 )
+
+// var: não se deve usar mais
+
+// let: valores que podem ser alterados posteriormente
+let contador = 0;
+contador += 1;
+console.log(contador)
+
+//const: valores que nunca se alteram (objetos, módulos)
+const cpf = 11122233344;
+console.log(cpf);
+
