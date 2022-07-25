0dd0c905a7260d44a6675bdc5025eded57d8b6cb
Lucas Estanislau <lucasestanislau@hotmail.com>
2022-07-25 14:44:07 -0300

    Strings e numbers

---
+++ capitulo01/demo02-strings-e-numbers.js
@@ -0,0 +1,17 @@
+let salarioAmigo = 1000
+let meuSalario = "2000.14"
+let meuSalarioCorreto = Number(meuSalario)
+console.log(salarioAmigo + meuSalarioCorreto)
+
+console.log(typeof(salarioAmigo), salarioAmigo)
+
+console.log(typeof(meuSalario), meuSalario)
+
+console.log(isNaN(meuSalario))
+
+let minhaString = "ola mundo"
+let minhaOutraString = "ola mundo 2"
+
+let minhaStringComVariaveis = `${minhaString} - ${minhaOutraString}. AE! `
+
+console.log(minhaStringComVariaveis);
