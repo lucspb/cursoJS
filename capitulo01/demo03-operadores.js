d75104fb587f1a6c8f99427525ff052b74fc7eff
Lucas Estanislau <lucasestanislau@hotmail.com>
2022-09-13 09:58:56 -0300

    operadores

---
+++ capitulo01/demo03-operadores.js
@@ -0,0 +1,37 @@
+//soma
+console.log("1+1 é: ", 1+1)
+
+//subtracao
+console.log("2-4 é: ", 2-4)
+
+//multiplicacao
+console.log("2*5 é: ", 2*5)
+
+//divisao
+console.log("10/2 é: ", 10/2)
+
+//resto
+console.log("2 % 5 é: ", 2%5)
+
+//exponencial (potencia)
+console.log("2**2 é ", 2**2)
+
+//COMPARAÇÃO
+console.log("\n-------------OPERADORES DE COMPARAÇÃO---------------\n")
+//maior que
+console.log("1>2 é:", 1>2)
+
+//menor que
+console.log("4<5 é:", 4<5)
+
+//igual
+console.log("1 === 2 é", 1 === 2)
+
+//diferente
+console.log("1 !== 2 é", 1 !== 2)
+
+//maior ou igual
+console.log("1 >= 2 é", 1 >= 2)
+
+//menor ou igual
+console.log("4 <= 4 é", 4<= 4)
